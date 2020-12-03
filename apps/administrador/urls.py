745dd0d6d24d83963ea9251d804a71189068efbc
Amairani Michel Duran <50429279+DMichelA@users.noreply.github.com>
2020-12-02 21:14:52 -0600

    Adjudicacion running

--- apps/administrador/urls.py
+++ apps/administrador/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 
-from apps.administrador.views import index, verificacion_bienes, bienes_adjudicar
+from apps.administrador.views import index, listado_bienes, bienes_adjudicar, bienes_adjudicados, \
+actualizar_bienes, eliminar_bienes
 
 urlpatterns = [
     path('', index, name="administrador_index"),
-    path('verificacion_bienes/', verificacion_bienes, name="verificacion_bienes"),
+    path('listado_bienes/', listado_bienes, name="listado_bienes"),
     path('bienes_adjudicar/<int:id_bien>/', bienes_adjudicar, name="adjudicion_bienes"),
+    path('bienes_adjudicados/', bienes_adjudicados, name="bienes_adjudicados"),
+    path('actualizar_bienes/<int:id_bien>/', actualizar_bienes, name="actualizar_bienes"),
+    path('eliminar_bienes/<int:id_bien>/', eliminar_bienes, name="eliminar_bienes"),
 ]
--- apps/administrador/views.py
+++ apps/administrador/views.py
@@ -7,10 +7,10 @@ from apps.usuario.models import Bienes
 def index(request):
     return render(request, 'administrador/index.html')
 
-def verificacion_bienes(request):
+def listado_bienes(request):
     bien = Bienes.objects.all().order_by('id')
     contexto = {'bienes':bien}
-    return render(request, 'administrador/verificacion.html', contexto)
+    return render(request, 'administrador/listado.html', contexto)
 
 def bienes_adjudicar(request, id_bien):
     bien = Bienes.objects.get(id=id_bien)
@@ -20,5 +20,31 @@ def bienes_adjudicar(request, id_bien):
         form = BienesForm(request.POST, instance=bien)
         if form.is_valid():
             form.save()
-        return redirect('verificacion_bienes')
+        return redirect('listado_bienes')
     return render(request, 'administrador/adjudicacion.html', {'form':form})
+
+def bienes_adjudicados(request):
+    bien = Bienes.objects.all().order_by('id')
+    contexto = {'bienes':bien}
+    return render(request, 'administrador/verificados.html', contexto)
+
+def actualizar_bienes(request, id_bien):
+    bien = Bienes.objects.get(id=id_bien)
+    if request.method == 'GET':
+        form = BienesForm(instance=bien)
+    else:
+        form = BienesForm(request.POST, instance=bien)
+        if form.is_valid():
+            form.save()
+        return redirect('listado_bienes')
+    return render(request, 'administrador/actualizacion.html', {'form':form})
+
+def eliminar_bienes(request, id_bien):
+    bien = Bienes.objects.get(id=id_bien)
+    if request.method == 'GET':
+        form = BienesForm(instance=bien)
+    else:
+        form = BienesForm(request.POST, instance=bien)
+        bien.delete()
+        return redirect('listado_bienes')
+    return render(request, 'administrador/eliminacion.html', {'form':form})
